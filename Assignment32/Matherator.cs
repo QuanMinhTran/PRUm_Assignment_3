a51d191d1fe2c40d108696f7dc7db4d5c7f3728d
Admin <woodiequan@gmail.com>
2022-02-25 21:57:51 +0700

    first init

---
+++ Assignment32/Matherator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Provides a variety of numeric methods
+    /// </summary>
+    public class Matherator
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Matherator()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Prints the numbers from 1 to 10
+        /// </summary>
+        public void PrintOneToTen()
+        {
+            Console.WriteLine("1 2 3 4 5 6 7 8 9 10 ");
+        }
+
+        /// <summary>
+        /// Prints the numbers from m to n
+        /// </summary>
+        /// <param name="m">m</param>
+        /// <param name="n">n</param>
+        public void PrintMToN(int m, int n)
+        {
+            string list = null;
+            for(int i =m; i<=n; i++)
+            {
+                list = list + i + " ";
+            }
+            Console.WriteLine(list);
+        }
+
+        /// <summary>
+        /// Returns the tenth even number, with 2 as the first even number
+        /// </summary>
+        /// <returns>tenth even number</returns>
+        public int GetTenthEvenNumber()
+        {
+            // delete code below; only included so we could compile
+            int num = 2;
+            for(int i = 1; i< 10; i++)
+            {
+                num += 2;
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// Returns the nth even number, with 2 as the first even number
+        /// </summary>
+        /// <param name="n">n</param>
+        /// <returns>nth even number</returns>
+        public int GetNthEvenNumber(int n)
+        {
+            // delete code below; only included so we could compile
+            int num = 2;
+            for (int i = 1; i < n; i++)
+            {
+                num += 2;
+            }
+            return num;
+        }
+
+        #endregion
+    }
+}
